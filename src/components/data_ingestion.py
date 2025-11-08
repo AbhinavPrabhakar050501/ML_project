e4d0ed9da9cb3a114ac18dc8cadbc3851d54aea9
Abhinav <abhinav2presidency@gmail.com>
2025-11-08 12:41:45 +0530

    Added data transformation pipeline.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,24 @@
+"""
+Read from a file 
+Break the file down into train test
+Store it in an artifacts folder
+"""
+
+
 import os
 import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass #used to define class variable directly without init
 class DataIngestionConfig:
@@ -52,4 +65,7 @@ class DataIngestion:
 if __name__ == "__main__":
     print(os.getcwd())
     obj = DataIngestion()
-    obj.initiate_data_ingestion()       
+    train_data, test_data = obj.initiate_data_ingestion()       
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
